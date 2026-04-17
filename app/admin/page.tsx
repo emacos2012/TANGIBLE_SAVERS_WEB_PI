349fe1d7138531634f460f1d14bb9484fa9a1fa4
emacos2012 <akakpoe431@gmail.com>
2026-04-17 17:29:54 +0100

    Create admin dashboard page

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+export default function AdminDashboard() {
+    return (
+        <div>
+            <h1>Admin Dashboard</h1>
+            <p>Welcome to the admin dashboard!</p>
+            {/* Add additional admin features and statistics here */}
+        </div>
+    );
+}
